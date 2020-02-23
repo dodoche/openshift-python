38a791f1e028e4570c388275788d90fa8a12d28b
dodoche <doralyfr@yahoo.fr>
2020-02-23 18:47:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python
 import openshift as oc
 
-print('OpenShift client version: {}'.format(oc.get_client_version()))
-print('OpenShift server version: {}'.format(oc.get_server_version()))
-
 # Set a project context for all inner `oc` invocations and limit execution to 10 minutes
 with oc.project('openshift-infra'), oc.timeout(10*60):
     # Print the list of qualified pod names (e.g. ['pod/xyz', 'pod/abc', ...]  in the current project
