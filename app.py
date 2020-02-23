13537b2c7588546b7b5409731ba958d2803e5291
dodoche <doralyfr@yahoo.fr>
2020-02-23 19:24:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python
+from __future__ import print_function
+from __future__ import absolute_import
 import openshift as oc
-print('alles richtig')
+from contextlib import contextmanager
+import
+import argparse
+import time
+import logging
+import traceback
+
+def main():
+
+  configmaps =  oc.selector("configmaps")
+  Configmaps = configmaps.objects()
+  conf = Configmaps[2]
+  print('Annotations:\n{}\n'.format(conf.model.metadata))
+
+if __name__ == '__main__':
+  main()
