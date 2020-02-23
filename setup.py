5f8210152c3bf21681183cd70f87e991adc4fd25
dodoche <doralyfr@yahoo.fr>
2020-02-23 18:04:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     name='openshift',
     author='doraly',
     url="https://github.com/dodoche/openshift-python.git/",
-    packages=find_packages('selector.py'),
-    description='Python sample application'
-    packages=find_packages()
+    packages=find_packages(),
+    description='Python sample application',
 )
