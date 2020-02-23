b102d0f11b8ee80bcf3e0001661e784e52e3c196
dodoche <doralyfr@yahoo.fr>
2020-02-23 12:09:38 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
-
-RUN yum --enablerepo=extras install -y epel-release && \
-    yum install -y git python3 python-pip && \
-    pip install paramiko pyyaml prometheus_client boto3 slackclient && \
-    mkdir /openshift-client-python
+RUN  pip install paramiko pyyaml prometheus_client boto3 slackclient && \
+    
+mkdir /openshift-client-python
 
 COPY packages /openshift-client-python/packages
 
