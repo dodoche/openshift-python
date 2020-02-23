019aeedc33193ec493f9a8e74bdaa63cb77540a8
dodoche <doralyfr@yahoo.fr>
2020-02-23 18:35:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,52 +1,31 @@
+#!/usr/bin/python
+import openshift as oc
 
-from __future__ import absolute_import
-from .selector import selector
-from .naming import qname_matches
-import unittest
+print('OpenShift client version: {}'.format(oc.get_client_version()))
+print('OpenShift server version: {}'.format(oc.get_server_version()))
 
-
-class TestSelector(unittest.TestCase):
-
-    def test_qname_matches(self):
-        self.assertTrue(qname_matches('template/x', 'template/x'))
-        self.assertTrue(qname_matches('template/x', ['template/x']))
-        self.assertTrue(qname_matches('template/x', ['template/y', 'template/x']))
-        self.assertFalse(qname_matches('template/x', ['template/y', 'template/x2']))
-
-        # See whether fuzzy matching of kinds is working
-
-        self.assertTrue(qname_matches('template/django', ['template.template.openshift.io/django']))
-        self.assertFalse(qname_matches('template/django', ['template.template.openshift.io/django.2']))
-        self.assertTrue(qname_matches('template/django', ['template.template/django']))
-        self.assertFalse(qname_matches('template/django', ['template.template/django.2']))
-
-        self.assertFalse(qname_matches('template2/django', ['template.template.openshift.io/django']))
-        self.assertFalse(qname_matches('template/django2', ['template.template.openshift.io/django']))
-
-    def test_set_operations(self):
-        s1 = selector([])
-        s2 = selector(['pod/abc', 'pod/xyz'])
-        self.assertEqual(s1.subtract(s2).qnames(), [])
-        self.assertEqual(s1.union(s2).qnames(), ['pod/abc', 'pod/xyz'])
-
-        s3 = selector(['pod/abc2', 'pod/xyz'])
-        self.assertEqual(s2.subtract(s3).qnames(), ['pod/abc'])
-        self.assertEqual(s2.intersect(s3).qnames(), ['pod/xyz'])
-
-        # See whether fuzzy matching of kinds is working
-
-        t1 = selector(['template/django'])
-        t2 = selector(['template.template.openshift.io/django', 'template.template.openshift.io/django2'])
-        self.assertEqual(len(t1.union(t2).qnames()), 2)
-        self.assertEqual(len(t1.intersect(t2).qnames()), 1)
-        self.assertEqual(len(t1.subtract(t2).qnames()), 0)
-
-        t1 = selector(['template/django'])
-        t2 = selector(['template.template.openshift.io/django'])
-        self.assertEqual(len(t1.union(t2).qnames()), 1)
-        self.assertEqual(len(t1.intersect(t2).qnames()), 1)
-        self.assertEqual(len(t1.subtract(t2).qnames()), 0)
-
-
-if __name__ == '__main__':
-    unittest.main()
+# Set a project context for all inner `oc` invocations and limit execution to 10 minutes
+with oc.project('openshift-infra'), oc.timeout(10*60):
+    # Print the list of qualified pod names (e.g. ['pod/xyz', 'pod/abc', ...]  in the current project
+    print('Found the following pods in {}: {}'.format(oc.get_project_name(), oc.selector('pods').qnames()))
+    
+    # Read in the current state of the pod resources and represent them as python objects
+    for pod_obj in oc.selector('pods').objects():
+        
+        # The APIObject class exposes several convenience methods for interacting with objects
+        print('Analyzing pod: {}'.format(pod_obj.name()))
+        pod_obj.print_logs(timestamps=True, tail=15)
+    
+        # If you need access to the underlying resource definition, get a Model instance for the resource
+        pod_model = pod_obj.model
+        
+        # Model objects enable dot notation and allow you to navigate through resources
+        # to an arbitrary depth without checking if any ancestor elements exist.
+        # In the following example, there is no need for boilerplate like:
+        #    `if .... 'ownerReferences' in pod_model['metadata'] ....`
+        # Fields that do not resolve will always return oc.Missing which 
+        # is a singleton and can also be treated as an empty dict.
+        for owner in pod_model.metadata.ownerReferences:  # ownerReferences == oc.Missing if not present in resource
+            # elements of a Model are also instances of Model or ListModel
+            if owner.kind is not oc.Missing:  # Compare as singleton
+                print '  pod owned by a {}'.format(owner.kind)  # e.g. pod was created by a StatefulSet
