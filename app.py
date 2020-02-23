6500e4fb33d8ee253c6bd824d94c315a30c5d2ee
dodoche <doralyfr@yahoo.fr>
2020-02-23 10:51:42 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+from __future__ import absolute_import
+import openshift as oc
+from contextlib import contextmanager
+import argparse
+import time
+import logging
+import traceback
+
+def main():
+
+  configmaps =  oc.selector("configmaps")
+  Configmaps = configmaps.objects()
+  conf = Configmaps[2]
+  print('Annotations:\n{}\n'.format(conf.model.metadata))
+
+if __name__ == '__main__':
+  main()
